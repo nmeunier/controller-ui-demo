07cfd699c5bd6f3eacd2d4193f62d16ef03dbc56
nmeunier <nicolas@nico-ner.fr>
2018-06-02 19:14:10 +0200

    Clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-// import { ClassicModule } from 'controller-ui';
+import { ClassicModule } from 'controller-ui';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,7 +14,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ClassicModule } from '../../projects/controller-ui/src/lib/classic/classic.module';
 
 @NgModule({
   declarations: [
